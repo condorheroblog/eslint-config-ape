73ae2205ea35fdf3ecbec99e73b2a0493e5f3fb5
CondorHero <love2xinwei@gmail.com>
2022-04-03 14:48:34 +0800

    feat: complete react package

--- packages/react/index.js
+++ packages/react/index.js
@@ -1,1 +1,15 @@
-module.exports = {};
+module.exports = {
+  extends: [
+    "plugin:react/recommended",
+    "plugin:react/jsx-runtime",
+    "plugin:react-hooks/recommended",
+    "plugin:jsx-a11y/recommended",
+    "@condorhero/eslint-config-typescript"
+  ],
+  settings: {
+    react: {
+      version: "18.0",
+    },
+  },
+};
+  
